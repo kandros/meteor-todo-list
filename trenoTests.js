288f345c723dd332836c96883bc29f8b79c31ae6
kandros <kandros5591@gmail.com>
2015-08-09 22:03:40 +0200

    initial commit

--- trenoTests.js
+++ trenoTests.js
@@ -4,7 +4,23 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1} });
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
+
+      // text is the name of the input
+      var inputText = event.target.text.value;
+
+      Tasks.insert({
+        text: inputText,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
     }
   });
 }
