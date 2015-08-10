5b4828e62cf1745fe01bd8b383ce58f94a71dfad
kandros <kandros5591@gmail.com>
2015-08-10 17:18:37 +0200

    initial commit

--- trenoTests.js
+++ trenoTests.js
@@ -4,7 +4,16 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      }
       return Tasks.find({}, {sort: {createdAt: -1} });
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -21,6 +30,9 @@ if (Meteor.isClient) {
       });
 
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+        Session.set("hideCompleted", event.target.checked);
     }
   });
 
